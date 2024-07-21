0676f87cce33b509585c72cc2374f43b235ac122
Sal Samani <sal@milysec.com>
2024-07-21 19:43:43 +1000

    tiplink int done

--- src/app/components/AppWalletProvider.tsx
+++ src/app/components/AppWalletProvider.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import React, { useMemo } from "react";
-import {
-    ConnectionProvider,
-    WalletProvider,
-} from "@solana/wallet-adapter-react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 // import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
-
+import { TipLinkWalletAdapter } from "@tiplink/wallet-adapter";
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
@@ -20,12 +17,17 @@ export default function AppWalletProvider({
 }: {
     children: React.ReactNode;
 }) {
-    const network = WalletAdapterNetwork.Devnet;
+    const network = WalletAdapterNetwork.Mainnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
     const wallets = useMemo(
         () => [
             // manually add any legacy wallet adapters here
             // new UnsafeBurnerWalletAdapter(),
+            new TipLinkWalletAdapter({
+                title: "Name of Dapp",
+                clientId: "694bf97c-d2ac-4dfc-a786-a001812658df",
+                theme: "dark"  // pick between "dark"/"light"/"system"
+            }),
         ],
         [network],
     );
